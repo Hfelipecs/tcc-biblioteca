082cc6675664709932879f6106af3cfcfbf3d22a
Hfelipecs <hfcostasilva@gmail.com>
2026-07-20 15:49:02 -0300

    voltar ao normal

--- src/main/java/com/app/demo/config/SecurityConfig.java
+++ src/main/java/com/app/demo/config/SecurityConfig.java
@@ -1,36 +1,50 @@
 package com.app.demo.config;
- 
+
 import com.app.demo.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
- 
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
- 
+
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
- 
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
- 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                // TEMPORARIO
-                .anyRequest().permitAll()
-            );
- 
-        return http.build();
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(HttpMethod.GET, "/livros/**").hasAnyRole("USUARIO", "BIBLIOTECARIO")
+            .requestMatchers(HttpMethod.GET, "/autores/**").hasAnyRole("USUARIO", "BIBLIOTECARIO")
+            .requestMatchers(HttpMethod.GET, "/editoras/**").hasAnyRole("USUARIO", "BIBLIOTECARIO")
+            .requestMatchers(HttpMethod.GET, "/emprestimos/**").hasAnyRole("USUARIO", "BIBLIOTECARIO")
+            .requestMatchers("/livros/**").hasRole("BIBLIOTECARIO")
+            .requestMatchers("/autores/**").hasRole("BIBLIOTECARIO")
+            .requestMatchers("/editoras/**").hasRole("BIBLIOTECARIO")
+            .requestMatchers("/emprestimos/**").hasRole("BIBLIOTECARIO")
+            .requestMatchers("/usuarios/**").hasRole("BIBLIOTECARIO")
+           .requestMatchers(HttpMethod.DELETE, "/BIBLIOTECARIOS/**").permitAll()
+            .requestMatchers("/enderecos/**").hasRole("BIBLIOTECARIO")
+            .anyRequest().authenticated()
+        )
+        .httpBasic(basic -> {});
+    return http.build();
     }
+
 }
